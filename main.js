12d5fe1cb61d1d7076cfe78fc38bf5031614280a
eunsu28 <ayay.e2008@gmail.com>
2021-12-20 14:58:05 +0800

    line test

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ ctx.fillStyle = 'brown';
 ctx.fillRect(0, 250, 1000, 500);
 
 //가운데다 화살표 만들기
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(300, 150);
+ctx.stroke();
 
 ctx.fillStyle = 'gray';
 ctx.beginPath();
