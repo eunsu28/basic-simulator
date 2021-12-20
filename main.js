7e9423efc191c1f543436447c9754ae10aa09592
eunsu28 <ayay.e2008@gmail.com>
2021-12-20 15:06:59 +0800

    making target sign finish

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'brown';
 ctx.fillRect(0, 250, 1000, 500);
 
-//가운데다 화살표 만들기
+ctx.lineWidth = 5;
 ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(300, 150);
+ctx.moveTo(450, 250);
+ctx.lineTo(550, 250);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(500, 200);
+ctx.lineTo(500, 300);
 ctx.stroke();
 
 ctx.fillStyle = 'gray';
