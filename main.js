026fddcf753f93967becdbb1a2053fd0a335c9ea
eunsu28 <ayay.e2008@gmail.com>
2021-12-26 21:12:28 +0800

    try to put a keyboard controlls

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const goundY = 250;
+let rightPressed = false;
+let leftPressed = false;
 
 ctx.fillStyle = 'brown';
-ctx.fillRect(0, 250, 1000, 500);
+ctx.fillRect(0, groundY, 1000, 500);
 
-ctx.lineWidth = 5;
-ctx.beginPath();
-ctx.moveTo(450, 250);
-ctx.lineTo(550, 250);
-ctx.stroke();
-ctx.beginPath();
-ctx.moveTo(500, 200);
-ctx.lineTo(500, 300);
-ctx.stroke();
+function draw(){
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.moveTo(450, 250);
+    ctx.lineTo(550, 250);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(500, 200);
+    ctx.lineTo(500, 300);
+    ctx.stroke();
 
-ctx.fillStyle = 'gray';
-ctx.beginPath();
-ctx.ellipse(500, 500, 250, 150, 0, Math.PI, true);
-ctx.fill();
+    ctx.fillStyle = 'gray';
+    ctx.beginPath();
+    ctx.ellipse(500, 500, 250, 150, 0, Math.PI, true);
+    ctx.fill();
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+
+if(rightPressed) {
+    goundY += 7;
+}
+else if(leftPressed) {
+    goundY -= 7;
+}
