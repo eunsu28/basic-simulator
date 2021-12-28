4278959d74115cae8792186728f00f38a2cd52a5
eunsu28 <ayay.e2008@gmail.com>
2021-12-28 17:46:32 +0800

    fix to keyboard work

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ let leftPressed = false;
 ctx.fillStyle = 'brown';
 ctx.fillRect(0, groundY, 1000, 500);
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 function draw(){
     ctx.lineWidth = 5;
     ctx.beginPath();
@@ -22,33 +25,33 @@ function draw(){
     ctx.beginPath();
     ctx.ellipse(500, 500, 250, 150, 0, Math.PI, true);
     ctx.fill();
-}
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
 
-function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
+    function keyDownHandler(e) {
+        if(e.key == "Right" || e.key == "ArrowRight") {
+            rightPressed = true;
+        }
+        else if(e.key == "Left" || e.key == "ArrowLeft") {
+            leftPressed = true;
+        }
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
+
+    function keyUpHandler(e) {
+        if(e.key == "Right" || e.key == "ArrowRight") {
+            rightPressed = false;
+        }
+        else if(e.key == "Left" || e.key == "ArrowLeft") {
+            leftPressed = false;
+        }
+    }
+
+
+    if(rightPressed) {
+        goundY += 7;
+    }
+    else if(leftPressed) {
+        goundY -= 7;
     }
 }
 
-function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = false;
-    }
-}
-
-
-if(rightPressed) {
-    goundY += 7;
-}
-else if(leftPressed) {
-    goundY -= 7;
-}
+setInterval(10, draw)
