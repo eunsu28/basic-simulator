41b1996d27f34d4cedeb8329201437edbae1dce0
eunsu28 <ayay.e2008@gmail.com>
2021-12-31 16:19:06 +0800

    change many things

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const groundY = 250;
-let rightPressed = false;
-let leftPressed = false;
-
-ctx.fillStyle = 'brown';
-ctx.fillRect(0, groundY, 1000, 500);
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+let groundY = 250;
+let upPressed = false;
+let downPressed = false;
 
 function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
+    if(e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = true;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
+    else if(e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = true;
     }
 }
 
 function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
+    if(e.key == "Up" || e.key == "ArrowUp") {
         rightPressed = false;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    else if(e.key == "Down" || e.key == "ArrowDown") {
         leftPressed = false;
     }
 }
 
 
 function draw(){
+
+    ctx.fillStyle = 'brown';
+    ctx.fillRect(0, groundY, 1000, 500);
+
     ctx.lineWidth = 5;
     ctx.beginPath();
     ctx.moveTo(450, 250);
@@ -45,13 +43,16 @@ function draw(){
     ctx.ellipse(500, 500, 250, 150, 0, Math.PI, true);
     ctx.fill();
 
+    if(upPressed) {
+        groundY += 7;
+    }
+    else if(downPressed) {
+        groundY -= 7;
+    }
+
 }
 
-if(rightPressed) {
-    groundY += 7;
-}
-else if(leftPressed) {
-    groundY -= 7;
-}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-setInterval(10, draw)
+setInterval(draw, 10)
